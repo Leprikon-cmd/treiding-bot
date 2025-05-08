bfe75f666b043fdcb237dd5ba3876dccf1607d6e
Leprikon <Zotchik.e.i@gmail.com>
2025-05-08 20:19:16 +0300

    Поправили логер

--- utils/logger.py
+++ utils/logger.py
@@ -1,19 +1,29 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# Убедимся, что папка для логов существует
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE = os.path.join(LOG_DIR, "trading_bot.log")
 
-file_handler = logging.FileHandler('logs/trading_bot.log', encoding='utf-8')
-file_handler.setLevel(logging.DEBUG)
+# Формат сообщений
+formatter = logging.Formatter("%(asctime)s - %(levelname)-8s - %(message)s")
+
+# Файловый хендлер с ротацией
+file_handler = RotatingFileHandler(
+    LOG_FILE, maxBytes=5*1024*1024, backupCount=3, encoding="utf-8"
+)
+file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 
+# Консольный хендлер
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 
-file_logger = logging.getLogger('file_logger')
-file_logger.setLevel(logging.DEBUG)
+# Единый логгер для файла и консоли
+file_logger = logging.getLogger("file_logger")
+file_logger.setLevel(logging.INFO)
 file_logger.addHandler(file_handler)
-
-console_logger = logging.getLogger('console_logger')
-console_logger.setLevel(logging.INFO)
-console_logger.addHandler(console_handler)
+file_logger.addHandler(console_handler)
